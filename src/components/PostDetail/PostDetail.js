e808ac5418df172248e17d698a46c0659289c22f
Jahid Hasan <slniloy500@gmail.com>
2021-03-11 20:04:05 +0600

    Social Buddy

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -21,7 +21,7 @@ const PostDetail = () => {
         fetch(url)
         .then(response => response.json())
         .then(data => setComments(data))
-    },[])
+    },[id])
 
     return (
         <div>
